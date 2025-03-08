8971bb0d760b978b421d218081b2f9e01f189127
Lokesh <94387299+gundalokesh@users.noreply.github.com>
2025-03-08 08:03:42 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,70 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductsComponent } from './products/products.component';
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, ProductsComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'web-app';
+  cards=['','','','','','','','']
+  gridtext=[
+    {
+      img:'download.jpg',
+      name:'one'
+    },
+    {
+      img:'download.jpg',
+      name:'one'
+    },
+    {
+      img:'download.jpg',
+      name:'one'
+    },
+    {
+      img:'download.jpg',
+      name:'one'
+    }
+
+   ]
+   products = [
+    { img: 'download.jpg', name: 'Zukes mini dog treats', description: "Zuke's Mini Naturals with Added Vitamins", originalPrice: 10, discountedPrice: 7, discount: 10, rating: 5.0, reviews: 123 },
+    { img: 'download.jpg', name: 'Zukes mini dog treats', description: "Zuke's Mini Naturals with Added Vitamins", originalPrice: 10, discountedPrice: 7, discount: 10, rating: 5.0, reviews: 123 },
+    { img: 'download.jpg', name: 'Zukes mini dog treats', description: "Zuke's Mini Naturals with Added Vitamins", originalPrice: 10, discountedPrice: 7, discount: 10, rating: 5.0, reviews: 123 },
+    { img: 'download.jpg', name: 'Zukes mini dog treats', description: "Zuke's Mini Naturals with Added Vitamins", originalPrice: 10, discountedPrice: 7, discount: 10, rating: 5.0, reviews: 123 },
+   
+   
+  
+  ];
+
+
+
+  bannermsg = [
+    {
+      productdis: 'Trending Product',
+      btntxt: 'Show All Products',
+      disc: '___ Everyday listed new products from our trusted seller ___'
+    }
+    ,
+    {
+      productdis: 'Offers on Top Brand',
+      btntxt: 'Show All Products',
+      disc: '___ Everyday listed new products from our trusted seller ___'
+    }
+    ,
+    {
+      productdis: 'Best Seling items',
+      btntxt: 'Show All Products',
+      disc: '___ Everyday listed new products from our trusted seller ___'
+    },
+    {
+      productdis: 'Browsing History',
+      btntxt: 'Show All Products',
+      disc: '___ Everyday listed new products from our trusted seller ___'
+    }
+  ]
 }
---
+++ src/app/products/products.component.ts
@@ -0,0 +1,31 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectorRef, Component, Input, OnInit } from '@angular/core';
+import { AppComponent } from '../app.component';
+
+@Component({
+  selector: 'app-products',
+  imports: [CommonModule],
+  templateUrl: './products.component.html',
+  styleUrl: './products.component.css'
+})
+export class ProductsComponent implements OnInit{
+  @Input() bannermessage: any;
+  @Input() products: any;
+
+  chunkedProducts: any[] = [];
+
+  constructor(private cdr: ChangeDetectorRef) {}
+
+  ngOnInit() {
+    if (this.products) {
+      this.chunkProducts(4);
+      this.cdr.detectChanges(); // Manually trigger change detection
+    }
+  }
+
+chunkProducts(chunkSize: number) {
+  for (let i = 0; i < this.products.length; i += chunkSize) {
+    this.chunkedProducts.push(this.products.slice(i, i + chunkSize));
+  }
+}
+}
